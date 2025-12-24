660dfdad8027fca744e4e291ba61d812e320add5
mikeru <javierjaypee8@gmail.com>
2025-12-24 17:11:54 +0800

    add dashboard card

--- frontend/src/views/Dashboard/Dashboard.vue
+++ frontend/src/views/Dashboard/Dashboard.vue
@@ -3,7 +3,7 @@ import { usePostStore } from '@/stores/postsStore';
 import { ref,watch, onMounted} from 'vue';
 import { Icon } from '@iconify/vue';
 import { useAuthStore } from '@/stores/authStore';
-
+import DashboardCard from './DashboardCard.vue';
 const PostStore = usePostStore();
 const AuthStore = useAuthStore();
 
@@ -16,17 +16,31 @@ onMounted(() => {
 </script>
 
 <template>
-    <main :class="PostStore.isDark ? 'bg-indigo-900 w-full min-h-screen text-white' : 'text-black'"class = "pt-39">
-        <section :class="PostStore.isDark? 'bg-indigo-800' : ''"class = ' ml-25 max-w-4xl rounded-2xl'>
+    <main :class="PostStore.isDark ? 'bg-indigo-900  text-white' : 'text-black'"class = "pt-35 w-full  min-h-screen">
+        <section :class="PostStore.isDark? 'bg-[#1e293b]' : 'shadow-md '"class = ' ml-25 w-auto mr-25 rounded-2xl'>
             <div class = 'flex p-5  items-center'>
-                <div class = 'bg-indigo-600 rounded-2xl p-4 ml-5 mr-5'>
+                <div class = 'bg-indigo-600 rounded-2xl p-4 mt-4 ml-5 mr-3'>
                     <Icon icon="mdi:chart-line" class="h-8 w-8 "/>
                 </div>
-                <div class = 'flex flex-col mt-4'>
-                    <h1 class ="text-3xl font-medium">Content Dashboard</h1>
-                    <h1 class = "text-gray-900"> Overview of your Digital Garden</h1>
+                <div class = 'flex flex-col mt-5'>
+                    <h1 class ="text-2xl font-normal mb-1">Content Dashboard</h1>
+                    <h1 class = "text-gray-800 text-sm"> Overview of your Digital Garden</h1>
                 </div>
             </div>
+            <section class = 'p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1'>
+                <DashboardCard 
+                    :background-color="PostStore.isDark ? 'bg-gradient-to-br from-indigo-900 to-indigo-800' : 'shadow-sm'"
+                    title="Tite"
+                    value="9"/>
+                <DashboardCard 
+                    :background-color="PostStore.isDark ? 'bg-gradient-to-br from-indigo-900 to-indigo-800' : 'shadow-sm'"
+                    title="Tite"
+                    value="9"/>
+                <DashboardCard 
+                    :background-color="PostStore.isDark ? 'bg-gradient-to-br from-indigo-900 to-indigo-800' : 'shadow-sm'"
+                    title="Tite"
+                    value="9"/>
+            </section>
         </section>
     </main>
     
---
+++ frontend/src/views/Dashboard/DashboardCard.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+import { defineProps } from 'vue';
+import { usePostStore } from '@/stores/postsStore';
+
+
+const PostStore = usePostStore();
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    value: {
+        type: [Number, String],
+        required: true
+    },
+    subtitle: {
+        type: String,
+        default: ''
+    },
+    icon: {
+        type: String,
+        required: true
+    },
+    iconColor: {
+        type: String,
+        default: 'text-blue-400'
+    },
+    backgroundColor: {
+        type: String,
+        default: 'bg-gray-800'
+    }
+});
+</script>
+
+<template>
+
+    <div :class="[props.backgroundColor, 'rounded-2xl p-4 shadow-xl max-w-sm  hover:shadow-2xl transition-all duration-300']">
+        <div class="flex justify-between items-start mb-8">
+            <h3 class=" text-base font-normal">{{ props.title }}</h3>
+            <Icon :icon="props.icon" :class="props.iconColor" class="h-9 w-9 opacity-80"/>
+        </div>
+        <div :class="[PostStore.isDark ? 'text-white' : '', 'mt-4']">
+            <h1 class=" text-6xl font-bold mb-3 tracking-tight">{{ props.value }}</h1>
+            <p class=" text-sm font-light">{{ props.subtitle || 'All time' }}</p>
+        </div>
+    </div>
+    
+</template>
