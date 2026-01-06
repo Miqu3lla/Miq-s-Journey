4cf278a27352172ba7c34d72ff395250652a4c08
mikeru <javierjaypee8@gmail.com>
2026-01-06 18:06:10 +0800

    cahnge axios timeout

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,7 +4,7 @@ const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000';
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
-    timeout: 10000,
+    timeout: 60000,
     headers: {
         'Content-Type': 'application/json',
     }
